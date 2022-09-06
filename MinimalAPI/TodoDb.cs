d308b58ad6b53945b76a0abed224717bc2e883e2
Equinox766 <Equinox766@gmail.com>
2022-09-06 15:07:31 -0400

    Agregar archivos de proyecto.

---
+++ MinimalAPI/TodoDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalAPI
+{
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options)
+            { }
+        public DbSet<Todo> Todos { get; set; }
+
+    }
+}
